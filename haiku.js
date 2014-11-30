0aee315da1becbf7e3ffdf1ff044555ee1a7e59d
Colin VanLang <colinvanlang@gmail.com>
2014-11-29 21:47:46 -0500

    Multiple words per line

--- haiku.js
+++ haiku.js
@@ -5,12 +5,12 @@ var fs = require("fs");
 var dictionary = {};
 // dictionary is an object that pairs # of syllables to an array of words
 
-// populate dictionary with cmudict
 fs.readFile('cmudict.txt', function(err, data) {
 	if(err) {
 		return console.log(err);
 	}
 
+	// populate dictionary with cmudict
 	var lines = data.toString().split("\n")
 	lines.forEach(function(line) {
 	  	line_split = line.split("  ");
@@ -23,21 +23,32 @@ fs.readFile('cmudict.txt', function(err, data) {
 		}
 	});
 
-	console.log(selectRandomWord("5", dictionary));
-	console.log(selectRandomWord("7", dictionary));
-	console.log(selectRandomWord("5", dictionary));
+	// Print out the haiku!
+	printLine(5);
+	printLine(7);
+	printLine(5);	
 });
 
 
 
+function printLine(syllables) {
+	var print_out = "";
+	while (syllables > 0) {
+		var word_syl = Math.ceil(Math.random() * syllables);
+		syllables -= word_syl;
+		print_out += selectRandomWord(word_syl) + " ";
+	}
+
+	console.log(print_out.substring(0, print_out.length - 1));
+}
+
 function countSyllables(phoneme) {
 	var syls = phoneme.match(/\d/g);
-	
 	if (syls) return syls.length;
 	else return 0;
 }
 
-function selectRandomWord(syllables, dictionary) {
+function selectRandomWord(syllables) {
 	var len = dictionary[syllables].length;
 	return dictionary[syllables][Math.floor(Math.random() * len)];
 }
